10f3aa891c35e1cab042ec3e4a53d788250fd1d4
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-19 15:35:52 -0400

    all tests fixed

--- test/models/dragon_test.rb
+++ test/models/dragon_test.rb
@@ -19,8 +19,8 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_young_dragons_from_asoiaf
     # use your knowledge of factories to make this test pass!
-    young_dragon = nil
-
+    young_dragon = build(:dragon, age:1)
+# binding.pry
     # not allowed to change the assert statement!
     skip unless young_dragon
     assert_equal( 'A Song of Ice and Fire', young_dragon.period_alive)
@@ -28,26 +28,27 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_middle_dragons_from_targ_dynasty
     # use your knowledge of factories to make this test pass!
-    middle_dragon = nil
+    middle_dragon = build(:dragon, age: 20)
 
     # not allowed to change the assert statement!
     skip unless middle_dragon
     assert_equal( 'Targaryen Dynasty', middle_dragon.period_alive)
   end
-
+# 'The Fall of the Dragons'
   def test_that_ancient_dragons_from_age_of_valyria
     # use your knowledge of factories to make this test pass!
-    ancient_dragon = nil
+    ancient_dragon = build(:dragon, age: 500)
 
     # not allowed to change the assert statement!
     skip unless ancient_dragon
     assert_equal( 'Age of Valyria', ancient_dragon.period_alive)
   end
+    #  'Targaryen Dynasty'
 
   def test_that_stranger_cannot_mount
     # use your knowledge of factories to make this test pass!
-    rider = nil
-    dragon = nil
+    rider = build(:rider, name:"Aegon")
+    dragon = build(:dragon, name:"Balerion")
 
     # not allowed to change the assert statement!
     skip unless rider && dragon
@@ -58,8 +59,8 @@ class DragonTest < ActiveSupport::TestCase
     # use your knowledge of factories to make this test pass!
     # Note: you'll need to do more than just supply the right names...
     # Hint: you can pass variables as properties too, not just strings!
-    rider = nil
-    dragon = nil
+    rider = build(:rider, name:"Aegon")
+    dragon = build(:dragon, name:"Balerion", rider: rider)
 
     # not allowed to change the assert statement!
     skip unless rider && dragon
--- test/models/rider_test.rb
+++ test/models/rider_test.rb
@@ -5,8 +5,8 @@ class RiderTest < ActiveSupport::TestCase
   def test_that_riders_swing
     # Use your knowledge of factories to make the test pass!
     # this time use create( ... ) instead of build
-    aegon = nil
-    daenerys = nil
+    aegon = Rider.create(name:"Aegon", sword:"Blackfyre", email:"aegon@email")
+    daenerys = Rider.create(name:"Daenerys", sword:"Lightbringer", email:"daenerys@email")
 
     # can't change the assert_ statements!
     skip unless aegon && daenerys
